5f911624d2f47b6b44841465f0f6ab4f08276b09
Hari Prasath M <143526410+64-78-888@users.noreply.github.com>
2024-06-24 16:27:58 +0530

    Create getlast.java

---
+++ getlast.java
@@ -0,0 +1,16 @@
+
+import java.util.LinkedList;
+
+class getlast{
+    public static void main(String[]args){
+        LinkedList<String> name =new LinkedList<>();
+        name.add("HARIPRASATH");
+        name.add("ARI");
+        name.add("MATHAN");
+        name.add("BALA");
+        name.add("GOWTHAM");
+        name.getLast();
+        System.out.println(name.getLast());
+
+    }
+}
